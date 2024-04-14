703202fe6ca1fca5852039e606eaae4ae1af6969
Mehebul02 <mehebul2123@gmail.com>
2024-04-14 19:58:18 +0600

    Page setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='font-poppins text-4xl font-bold'>Hospitality</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
